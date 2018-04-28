f53f4e587342343fa79de84700afae989426468c
Thomas Roberts <t.92.roberts@gmail.com>
2018-04-28 19:18:46 +0200

    ViewPager for the main menu
    Able to swipe between fragments in the main menu

---
+++ app/src/main/java/roberts/thomas/bikeshare2/view/MainMenuPagerActivity.java
@@ -0,0 +1,45 @@
+package roberts.thomas.bikeshare2.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+
+import roberts.thomas.bikeshare2.R;
+
+/**
+ * Created by Tom on 27/04/2018.
+ */
+
+public class MainMenuPagerActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_view_pager);
+
+        mViewPager = findViewById(R.id.view_pager);
+
+        // Add the fragments that will be paged to the ArrayList
+        final ArrayList<Fragment> fragments = new ArrayList<>();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+    }
+}
