d47131f3c9195458d79760de38d8c1f09018e54b
Thomas Roberts <t.92.roberts@gmail.com>
2018-04-29 17:19:49 +0200

    Layour for a single Customer
    View the details of a single Customer

--- app/src/main/java/roberts/thomas/bikeshare2/model/Customer.java
+++ app/src/main/java/roberts/thomas/bikeshare2/model/Customer.java
@@ -22,18 +22,11 @@ public class Customer extends RealmObject {
         mAccountBalance = MutableRealmInteger.valueOf(0);
     }
 
-    public Customer(String id, String firstName, String lastName) {
+    public Customer(String id, String firstName, String lastName, int accountBalance) {
         mId = id;
         mFirstName = firstName;
         mLastName = lastName;
-        mAccountBalance = MutableRealmInteger.valueOf(0);
-    }
-
-    public Customer(String id, String firstName, String lastName, int startingBalance) {
-        mId = id;
-        mFirstName = firstName;
-        mLastName = lastName;
-        mAccountBalance = MutableRealmInteger.valueOf(startingBalance);
+        mAccountBalance = MutableRealmInteger.valueOf(accountBalance);
     }
 
     public String getFullName() {
